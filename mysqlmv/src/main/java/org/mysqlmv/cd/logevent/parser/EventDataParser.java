cc25d8ea2272538fe35735a3c797ba86c903bc43
Kelvinli1988 <lqecnu@gmail.com>
2014-11-17 17:19:09 +0800

    add table parser and test file

--- mysqlmv/src/main/java/org/mysqlmv/cd/logevent/parser/EventDataParser.java
+++ mysqlmv/src/main/java/org/mysqlmv/cd/logevent/parser/EventDataParser.java
@@ -9,5 +9,5 @@ import java.io.IOException;
  * Created by Kelvin Li on 11/13/2014 10:54 AM.
  */
 public interface EventDataParser<T extends EventData> {
-    T parse(ByteArrayInputStream stream) throws IOException;
+    T parse(ByteArrayInputStream input) throws IOException;
 }
