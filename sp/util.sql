6066c86ffd992670560146ccf238f31fbba45b31
qiang li <lqecnu@gmail.com>
2014-12-28 17:02:05 +0800

    util.sql

---
+++ sp/util.sql
@@ -0,0 +1,13 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS mysqlmv.util_concat //
+
+CREATE DEFINER=root@localhost PROCEDURE mysqlmv.util_concat(
+  sql TEXT,
+  seperator CHAR
+)
+BEGIN
+
+END //
+
+DELIMITER ;
